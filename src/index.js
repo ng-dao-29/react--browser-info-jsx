b15cde8e6b1eaada04e0a967c49035fc66aaa907
ng-dao-29 <112545575+ng-dao-29@users.noreply.github.com>
2022-12-29 23:03:22 +0700

    exercice

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <h4>Browser's details: {navigator.userAgent}</h4>
 );
 
 // If you want to start measuring performance in your app, pass a function
